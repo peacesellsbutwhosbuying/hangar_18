a76e73b09dd68b92b826679466ce52277951b88e
peacesellsbutwhosbuying <jnreey547@gmail.com>
2020-11-14 15:58:27 +0700

    v_0.2

--- sprites.py
+++ sprites.py
@@ -11,12 +11,20 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
 
         pygame.sprite.Sprite.__init__(self)
-        # Загружаем изображение персонажа
-        self.image = pygame.image.load(r'Vic_front.png').convert_alpha()
+        # Переменная ходьбы
+        self.walking = False
+        # Переменная прыжка
+        self.jumping = False
+        # Текущий кадр
+        self.current_frame = 0
+        # Последний обновлённый кадр
+        self.last_update = 0
+        # Загружаем изображение персонажа через функцию
+        self.load_images()
+        self.image = self.standing
         # Получаем область персонажа
         self.rect = self.image.get_rect()
         # Удаляем фон, отсавляя только модель персонажа
-        self.image.set_colorkey(back_gr)
         # Указываем начальную позицию персонажа
         self.rect.center = (WIDTH//2, HEIGHT//2)
         # Указываем начальную позицию игрока, но через вектор
@@ -26,8 +34,32 @@ class Player(pygame.sprite.Sprite):
         # Ускорение игрока
         self.ac = vector2(0, 0)
 
+    def load_images(self):
+        """Функция загрузки изображений персонажа"""
+        # Изображения когда персонаж стоит
+        self.standing = pygame.image.load('Vic_front.png')
+        # Удаление фона изображения
+        self.standing.set_colorkey(back_gr)
+        # Изображения, если игрок идёт вправо
+        self.walk_right_frames = [pygame.image.load('Right_1.png'),
+                                 pygame.image.load('Right_2.png'),
+                                 pygame.image.load('Right_3.png'),
+                                 pygame.image.load('Right_4.png')]
+        # Удаление фона для анимации передвижения вправо
+        for frame in self.walk_right_frames:
+            frame.set_colorkey(back_gr)
+        """Переворот изображений для анимации движения влево"""
+        # Создаём пустой массив
+        self.walk_left_frames = []
+        for frame in self.walk_left_frames:
+            frame.set_colorkey(back_gr)
+        # Заполняем массив развёрнутыми изображениями
+        for frame in self.walk_right_frames:
+            self.walk_left_frames.append(pygame.transform.flip(frame, True, False))
+
     def update(self):
         """Функция передвижения игрока в пространстве"""
+        self.animate()
         self.ac = vector2(0, 1.15)
         # Считывание всех нажатых клавиш
         self.keys = pygame.key.get_pressed()
@@ -41,9 +73,38 @@ class Player(pygame.sprite.Sprite):
         self.ac += self.vel * player_friction
         # Расчёт передвижения игрока
         self.vel += self.ac
+        # Условие для остановки анимации
+        if abs(self.vel.x) < 0.5:
+            self.vel.x = 0
+
         self.pos += self.vel + 0.5 * self.ac
 
         self.rect.midbottom = self.pos
+
+    def animate(self):
+        """Функция анимации игрока"""
+        # Время игры
+        now = pygame.time.get_ticks()
+
+        if self.vel.x != 0:
+            self.walking = True
+        else:
+            self.walking = False
+        # Анимация персонажа
+        if self.walking:
+            if now - self.last_update > 200:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.walk_right_frames)
+                bottom = self.rect.bottom
+                if self.vel.x > 0:
+                    self.image = self.walk_right_frames[self.current_frame]
+                else:
+                    self.image = self.walk_left_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+
+        if not self.walking:
+            self.image = self.standing
 
 
 class Platform(pygame.sprite.Sprite):
